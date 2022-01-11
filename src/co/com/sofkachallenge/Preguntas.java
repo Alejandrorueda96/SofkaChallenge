f33974151fc1373a3c664cb4ae48b89f2fd496e1
Alejandro Rueda <alejandrorueda96@hotmail.com>
2022-01-10 20:41:06 -0500

    Creacion de la clase Pregunta junto con sus variables principales

---
+++ src/co/com/sofkachallenge/Preguntas.java
@@ -0,0 +1,11 @@
+package co.com.sofkachallenge;
+import java.util.Random;
+
+public class Preguntas {
+	private int dificultad; //Variable para almacenar la dificultad 
+	private int ronda; //Variable para almacenar la ronda
+	private int pregunta; //Variable para almacenar la pregunta escojida aleatoriamente
+	private int respuesta; //Variable para almacenar la respuesta ingresada por el usuario
+	Random random = new Random(); //Clase para generar un numero aleatorio 
+	
+}
