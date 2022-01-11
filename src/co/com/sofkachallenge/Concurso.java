a6b98ef811f6c6f91c237569524d58661e895e6f
Alejandro Rueda <alejandrorueda96@hotmail.com>
2022-01-10 23:22:26 -0500

    se le asigna al jugador un premio por cada respuesta correcta

--- src/co/com/sofkachallenge/Concurso.java
+++ src/co/com/sofkachallenge/Concurso.java
@@ -5,6 +5,7 @@ public class Concurso {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner jugadorInput = new Scanner(System.in); //Clase para obtener la entrada del usuario
+		int[] premios = {100, 200, 300, 400, 500};
 		//Crear un nuevo jugador
 		Jugador jugador = new Jugador();
 		Preguntas preguntas = new Preguntas();
@@ -24,8 +25,10 @@ public class Concurso {
 				preguntas.setRespuesta(Integer.parseInt(jugadorInput.nextLine()));
 				if (preguntas.validarRespuesta() == true) {
 					System.out.println("Respuesta correcta, avanza a la siguiente ronda");
+					jugador.setPremioAcumulado(jugador.getPremioAcumulado()+premios[preguntas.getDificultad()]);
 					preguntas.setRonda(preguntas.getRonda() + 1);
 					preguntas.setDificultad(preguntas.getDificultad() + 1);
+					System.out.println("premio " + jugador.getPremioAcumulado());
 				}
 				else {
 					System.out.println("Respuesta incorrecta juego terminado");
