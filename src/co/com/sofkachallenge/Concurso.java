50df4483e86e86f32a504912617879dc2a7f3346
Alejandro Rueda <alejandrorueda96@hotmail.com>
2022-01-10 21:05:16 -0500

    Habilitar la consola para tomar los valores de entrada del jugador

--- src/co/com/sofkachallenge/Concurso.java
+++ src/co/com/sofkachallenge/Concurso.java
@@ -7,7 +7,10 @@ public class Concurso {
 		Scanner jugadorInput = new Scanner(System.in); //Clase para obtener la entrada del usuario
 		//Crear un nuevo jugador
 		Jugador jugador = new Jugador();
-		jugador.setNombre("Alejandro");
+		//El jugador puede ingresar su nombre por medio de la consola 
+		System.out.print("Nombre:");
+		jugador.setNombre(jugadorInput.nextLine()); 
+		
 		System.out.println(jugador.getNombre());
 		
 		
