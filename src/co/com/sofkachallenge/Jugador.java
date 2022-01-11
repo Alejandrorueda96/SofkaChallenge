c114bfbcc65167cab418eff9c748b4410ee22245
Alejandro Rueda <Alejandro Rueda@DESKTOP-3LEDK4L>
2022-01-10 20:23:40 -0500

    Creacion de getters y setters

--- src/co/com/sofkachallenge/Jugador.java
+++ src/co/com/sofkachallenge/Jugador.java
@@ -2,7 +2,30 @@ package co.com.sofkachallenge;
 
 public class Jugador {
 
+	//Variables
 	private String nombre;
 	private String apellido;
-	private int premio;
+	private int premioAcumulado;
+	
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public int getPremioAcumulado() {
+		return premioAcumulado;
+	}
+	public void setPremioAcumulado(int premioAcumulado) {
+		this.premioAcumulado = premioAcumulado;
+	}
+	
+	
 }
